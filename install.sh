e1b2c98593d8b54df3f504ac9c244e713f1cf562
EHSAN ETESAMI <eh.etesami@gmail.com>
2021-11-20 12:31:26 -0500

    Prepare to be distributed.

--- install.sh
+++ install.sh
@@ -13,12 +13,13 @@ conda init bash
 conda env create -f environment.yml # Install dependencies
 conda activate fedscale
 
+echo "source activate fedscale" | tee -a ~/.bashrc
 
 # un-comment to install cuda
-wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
-sudo apt-get purge nvidia-* -y
-sudo sh -c "echo 'blacklist nouveau\noptions nouveau modeset=0' > /etc/modprobe.d/blacklist-nouveau.conf"
-sudo update-initramfs -u
-sudo sh cuda_10.2.89_440.33.01_linux.run --override --driver --toolkit --samples --silent
-export PATH=$PATH:/usr/local/cuda-10.2/
-conda install cudatoolkit=10.2 -y
+# wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
+# sudo apt-get purge nvidia-* -y
+# sudo sh -c "echo 'blacklist nouveau\noptions nouveau modeset=0' > /etc/modprobe.d/blacklist-nouveau.conf"
+# sudo update-initramfs -u
+# sudo sh cuda_10.2.89_440.33.01_linux.run --override --driver --toolkit --samples --silent
+# export PATH=$PATH:/usr/local/cuda-10.2/
+# conda install cudatoolkit=10.2 -y
